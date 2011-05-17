ddf46c89a9c93b9ad90749897556ffc87bd2a0a0
Changho Lee <changholee@Changho-Leeui-iMac.local>
2011-05-17 20:03:01 +0900

    Initial Commit

---
+++ Summing/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Summing
+//
+//  Created by Changho Lee on 11. 5. 17..
+//  Copyright 2011 타임교육. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ Summing/SummingAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SummingAppDelegate.h
+//  Summing
+//
+//  Created by Changho Lee on 11. 5. 17..
+//  Copyright 2011 타임교육. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummingAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
